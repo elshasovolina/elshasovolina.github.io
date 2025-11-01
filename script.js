adcf0d29afc9963cc0a24a821ee211e46b439196
elshasovolina <elsha.sovolina@gmail.com>
2025-11-01 16:50:28 +0700

    mobile style

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ document.querySelectorAll('a[href^="#"]').forEach(a=>{
     }
   });
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const navToggle = document.getElementById("nav-toggle");
+  const mainNav = document.getElementById("main-nav");
+
+  navToggle.addEventListener("click", () => {
+    mainNav.classList.toggle("active");
+  });
+});
